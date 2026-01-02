6300bd249a44f1eee220ed9cef033f9965dd7c6e
PankajKumarBehera12 <pankajkumarbehera083@gmail.com>
2026-01-02 22:50:53 +0530

    adding home, login, register

--- herbalist/urls.py
+++ herbalist/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout_view/', views.logout_view, name='logout_view'),
+    path('home/', views.home, name='home'),
 ]
